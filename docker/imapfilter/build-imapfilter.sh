fd67d47488791286a60bad5839e6c66eecae4a75
Andrew Clemons <andrew.clemons@gmail.com>
2023-08-13 13:59:02 +0900

    Build an image to run imapfilter from a lambda function.
    Push this into the ECR repo we created previously.

---
+++ docker/imapfilter/build-imapfilter.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Copyright 2023 Andrew Clemons, Tokyo Japan
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+set -e
+set -o pipefail
+
+# terse package install for installpkg
+export TERSE=0
+
+slackpkg -default_answer=yes -batch=on update
+
+EXIT_CODE=0
+slackpkg -default_answer=yes -batch=on install make guile gc gcc-11 glibc kernel-headers binutils || EXIT_CODE=$?
+
+if [ $EXIT_CODE -ne 0 ] && [ $EXIT_CODE -ne 20 ] ; then
+  exit $EXIT_CODE
+fi
+
+rm -rf /var/cache/packages/* && rm -rf /var/lib/slackpkg/*
+
+# renovate: datasource=github-tags depName=SlackBuildsOrg/slackbuilds versioning=loose
+SBO_RELEASE_VERSION="15.0-20230715.1"
+wget -O - "https://github.com/SlackBuildsOrg/slackbuilds/tarball/$SBO_RELEASE_VERSION" | tar xz
+
+export TAG=_aclemons
+export PKGTYPE=txz
+(
+  cd SlackBuildsOrg-slackbuilds-*
+
+  cd development/lua/
+
+  . lua.info
+
+  wget "$DOWNLOAD"
+  printf "%s\t%s\n" "$MD5SUM" "$(basename "$DOWNLOAD")" | md5sum --check --quiet
+  bash lua.SlackBuild
+)
+
+installpkg /tmp/*.txz
+
+(
+  cd SlackBuildsOrg-slackbuilds-*
+
+  cd network/imapfilter
+
+  . imapfilter.info
+
+  wget "$DOWNLOAD"
+  printf "%s\t%s\n" "$MD5SUM" "$(basename "$DOWNLOAD")" | md5sum --check --quiet
+  bash imapfilter.SlackBuild
+)
+
+rm -rf SlackBuildsOrg-slackbuilds-*
+rm -rf /tmp/SBo
---
+++ docker/imapfilter/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+printf 'Starting lambda handler. Running as pid %s\n' "$BASHPID"
+
+shutdown() {
+  printf 'Shutting down gracefully\n'
+  exit
+}
+trap 'shutdown' SIGTERM
+
+while true ; do
+  HEADERS="$(mktemp)"
+  BODY="$(mktemp)"
+  curl -f -sS -LD "$HEADERS" -X GET "http://$AWS_LAMBDA_RUNTIME_API/2018-06-01/runtime/invocation/next" -o "$BODY"
+  REQUEST_ID="$(grep -Fi Lambda-Runtime-Aws-Request-Id "$HEADERS" | tr -d '[:space:]' | cut -d: -f2)"
+  rm -rf "$HEADERS"
+
+  printf 'Processing request %s\n' "$REQUEST_ID"
+
+  ret=0
+  imapfilter -v || ret=$?
+
+  if [ $ret -eq 0 ] ; then
+    curl -f -sS -X POST "http://$AWS_LAMBDA_RUNTIME_API/2018-06-01/runtime/invocation/$REQUEST_ID/response" -d "{\"result\": 0}"
+  else
+    curl -f -sS -X POST "http://$AWS_LAMBDA_RUNTIME_API/2018-06-01/runtime/invocation/$REQUEST_ID/response" -d "{\"result\": $ret}"
+  fi
+  rm -rf "$BODY"
+done
